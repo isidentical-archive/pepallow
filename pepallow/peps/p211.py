4e1d81ef79e99ecfce92f834cf28dd03af085b78
Muhammed Alkan <dev.mhlkan@gmail.com>
2019-04-07 07:50:47 +0300

    Bugfixes & New peps (#1)
    * Fix bugs & correct output of PEP 211
    
    * Add tuple length control
    
    * Add PEP276

--- pepallow/peps/p211.py
+++ pepallow/peps/p211.py
@@ -16,8 +16,20 @@ class PEP211Transformer(ast.NodeTransformer):
     """
 
     def visit_For(self, node):
-        if isinstance(node.iter, ast.BinOp) and isinstance(node.iter.op, ast.MatMult):
-            node.iter = ast.Call(
-                ast.Name("zip", ast.Load()), [node.iter.left, node.iter.right], []
+        if (
+            isinstance(node.target, ast.Tuple)
+            and isinstance(node.iter, ast.BinOp)
+            and isinstance(node.iter.op, ast.MatMult)
+        ):
+            if len(node.target.elts) != 2:
+                raise ValueError("There is should be only 2 names to unpack.")
+            a, b = node.target.elts
+            return ast.For(
+                target=a,
+                iter=node.iter.left,
+                body=[
+                    ast.For(target=b, iter=node.iter.right, body=node.body, orelse=[])
+                ],
+                orelse=node.orelse,
             )
         return node
